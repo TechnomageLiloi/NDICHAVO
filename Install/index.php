b7d5afd7bd28aa4f7c872ec6dcaf368ed2be6e2b
Anton Moskalenko <moskalenko.anton@gmail.com>
2026-02-26 17:51:29 +0200

    [0.00.001] Add Install block.

---
+++ Install/index.php
@@ -0,0 +1,16 @@
+<?php
+
+ini_set('display_errors', 'On');
+session_start();
+error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
+
+define('ROOT_URL', '');
+define('ROOT_PATH', __DIR__);
+
+include_once __DIR__ . '/TARDIS.phar';
+
+$app = new \Liloi\Paper\Application([
+    'root' => __DIR__ . '/Sandbox' // Here you must change to real folder
+]);
+
+echo $app->compile();
